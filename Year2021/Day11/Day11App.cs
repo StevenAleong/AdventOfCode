a0cfd360bfbe5f3824cfbbac96107a58ff4f55be
Steven Aleong <stevenaleong@outlook.com>
2021-12-11 14:50:17 -0700

    Mixed up X and Y's

--- Year2021/Day11/Day11App.cs
+++ Year2021/Day11/Day11App.cs
@@ -31,7 +31,7 @@ namespace AdventOfCode.Year2021
                 {
                     for (var y = 0; y < dumbos[x].Count(); y++)
                     {
-                        UpDumboPosition(ref dumbos, x, y);
+                        UpDumboPosition(ref dumbos, y, x);
                     }
                 }
 
@@ -51,11 +51,11 @@ namespace AdventOfCode.Year2021
         private bool DidAllFlashAtSameTime(ref List<List<int>> dumbos)
         {
             var count = 0;
-            for (var x = 0; x < dumbos.Count(); x++)
+            for (var y = 0; y < dumbos.Count(); y++)
             {
-                for (var y = 0; y < dumbos[x].Count(); y++)
+                for (var x = 0; x < dumbos[y].Count(); x++)
                 {
-                    if (dumbos[x][y] == 0)
+                    if (dumbos[y][x] == 0)
                         count++;
                 }
             }
@@ -68,16 +68,16 @@ namespace AdventOfCode.Year2021
             Console.WriteLine("------------------------------------");
             Console.WriteLine($"Day {day}");
 
-            for (var x = 0; x < dumbos.Count(); x++)
+            for (var y = 0; y < dumbos.Count(); y++)
             {
-                for (var y = 0; y < dumbos[x].Count(); y++)
+                for (var x = 0; x < dumbos[y].Count(); x++)
                 {
-                    if (dumbos[x][y] == 0)
+                    if (dumbos[y][x] == 0)
                         Console.ForegroundColor = ConsoleColor.Red;
 
-                    Console.Write($"{dumbos[x][y]}");
+                    Console.Write($"{dumbos[y][x]}");
 
-                    if (dumbos[x][y] == 0)
+                    if (dumbos[y][x] == 0)
                         Console.ResetColor();
 
                 }
@@ -87,55 +87,55 @@ namespace AdventOfCode.Year2021
 
         private void ResetAllFlashed(ref List<List<int>> dumbos)
         {
-            for (var x = 0; x < dumbos.Count(); x++)
+            for (var y = 0; y < dumbos.Count(); y++)
             {
-                for (var y = 0; y < dumbos[x].Count(); y++)
+                for (var x = 0; x < dumbos[y].Count(); x++)
                 {
-                    if (dumbos[x][y] > 9)
-                        dumbos[x][y] = 0;
+                    if (dumbos[y][x] > 9)
+                        dumbos[y][x] = 0;
                 }
             }
         }
 
-        private void UpDumboPosition(ref List<List<int>> dumbos, int x, int y)
+        private void UpDumboPosition(ref List<List<int>> dumbos, int y, int x)
         {
-            dumbos[x][y]++;
+            dumbos[y][x]++;
 
-            if (dumbos[x][y] == 10)
+            if (dumbos[y][x] == 10)
             {
                 this.Flashes++;
 
                 // Up Left
-                if (x - 1 >= 0 && y - 1 >= 0)
-                    UpDumboPosition(ref dumbos, x - 1, y - 1);
+                if (y - 1 >= 0 && x - 1 >= 0)
+                    UpDumboPosition(ref dumbos, y - 1, x - 1);
 
                 // Up
-                if (x - 1 >= 0)
-                    UpDumboPosition(ref dumbos, x - 1, y);
+                if (y - 1 >= 0)
+                    UpDumboPosition(ref dumbos, y - 1, x);
 
                 // Up Right
-                if (x - 1 >= 0 && y + 1 < dumbos[x].Count())
-                    UpDumboPosition(ref dumbos, x - 1, y + 1);
+                if (y - 1 >= 0 && x + 1 < dumbos[y].Count())
+                    UpDumboPosition(ref dumbos, y - 1, x + 1);
 
                 // Left
-                if (y - 1 >= 0)
-                    UpDumboPosition(ref dumbos, x, y - 1);
+                if (x - 1 >= 0)
+                    UpDumboPosition(ref dumbos, y, x - 1);
 
                 // Right
-                if (y + 1 < dumbos[x].Count())
-                    UpDumboPosition(ref dumbos, x, y + 1);
+                if (x + 1 < dumbos[y].Count())
+                    UpDumboPosition(ref dumbos, y, x + 1);
 
                 // Down Left
-                if (x + 1 < dumbos.Count() && y - 1 >= 0)
-                    UpDumboPosition(ref dumbos, x + 1, y - 1);
+                if (y + 1 < dumbos.Count() && x - 1 >= 0)
+                    UpDumboPosition(ref dumbos, y + 1, x - 1);
 
                 // Down
-                if (x + 1 < dumbos.Count())
-                    UpDumboPosition(ref dumbos, x + 1, y);
+                if (y + 1 < dumbos.Count())
+                    UpDumboPosition(ref dumbos, y + 1, x);
 
                 // Down Right
-                if (x + 1 < dumbos.Count() && y + 1 < dumbos[x].Count())
-                    UpDumboPosition(ref dumbos, x + 1, y + 1);
+                if (y + 1 < dumbos.Count() && x + 1 < dumbos[y].Count())
+                    UpDumboPosition(ref dumbos, y + 1, x + 1);
 
             }
         }
@@ -143,145 +143,3 @@ namespace AdventOfCode.Year2021
     }
 
 }
-
-
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace AdventOfCode.Year2021
-//{
-//    internal class Day11App
-//    {
-//        private List<string> Input = AppExtensions.GetInputList($"./Year2021/Day11/Input.txt");
-
-//        private int Flashes = 0;
-
-//        public void Execute()
-//        {
-//            Part1();
-//        }
-
-//        private void Part1()
-//        {
-//            var dumbos = new List<List<Day11Dumbo>>();
-
-//            // Gather data into a grid
-//            for (var i = 0; i < this.Input.Count(); i++)
-//            {
-//                dumbos.Add(this.Input[i].ToCharArray().Select(m => new Day11Dumbo() { EnergyLevel = Convert.ToInt32(m.ToString()) }).ToList());
-//            }
-
-//            var daysToDo = 100;
-
-//            for (var day = 1; day <= daysToDo; day++)
-//            {
-
-//                for (var x = 0; x < dumbos.Count(); x++)
-//                {
-//                    for (var y = 0; y < dumbos[x].Count(); y++)
-//                    {
-//                        UpDumboPosition(ref dumbos, x, y);                       
-//                    }
-//                }
-
-//                // Reset all the dumbos to check if they've flashed
-//                ResetAllFlashed(ref dumbos);
-
-//                DisplayCurrentDumbos(ref dumbos, day);
-//            }
-
-//            Console.WriteLine($"Part 1 Flash Count: {this.Flashes}");
-//        }
-
-//        private void DisplayCurrentDumbos(ref List<List<Day11Dumbo>> dumbos, int day) {
-//            Console.WriteLine("------------------------------------");
-//            Console.WriteLine($"Day {day}");
-
-//            for (var x = 0; x < dumbos.Count(); x++)
-//            {
-//                for (var y = 0; y < dumbos[x].Count(); y++)
-//                {
-//                    if (dumbos[x][y].EnergyLevel == 0)
-//                        Console.ForegroundColor = ConsoleColor.Red;
-
-//                    Console.Write($"{dumbos[x][y].EnergyLevel}");
-
-//                    if (dumbos[x][y].EnergyLevel == 0)
-//                        Console.ResetColor();
-
-//                }
-//                Console.WriteLine("");
-//                //Console.WriteLine(String.Join("", dumbos[x].Select(m => m.EnergyLevel).ToList()));
-//            }
-//        }
-
-//        private void ResetAllFlashed(ref List<List<Day11Dumbo>> dumbos)
-//        {
-//            for (var x = 0; x < dumbos.Count(); x++)
-//            {
-//                for (var y = 0; y < dumbos[x].Count(); y++)
-//                {
-//                    dumbos[x][y].Flashed = false;
-//                    if (dumbos[x][y].EnergyLevel > 9)
-//                    {
-//                        dumbos[x][y].EnergyLevel = 0;
-//                    }
-//                }
-//            }
-//        }
-
-//        private void UpDumboPosition(ref List<List<Day11Dumbo>> dumbos, int x, int y)
-//        {
-//            dumbos[x][y].EnergyLevel++;
-
-//            if (dumbos[x][y].EnergyLevel >= 10 && dumbos[x][y].Flashed == false)
-//            {
-//                this.Flashes++;
-//                dumbos[x][x].Flashed = true;
-
-//                // Up Left
-//                if (x - 1 >= 0 && y - 1 >= 0)
-//                    UpDumboPosition(ref dumbos, x - 1, y - 1);
-
-//                // Up
-//                if (x - 1 >= 0)
-//                    UpDumboPosition(ref dumbos, x - 1, y);
-
-//                // Up Right
-//                if (x - 1 >= 0 && y + 1 < dumbos[x].Count())
-//                    UpDumboPosition(ref dumbos, x - 1, y + 1);
-
-//                // Left
-//                if (y - 1 >= 0)
-//                    UpDumboPosition(ref dumbos, x, y - 1);
-
-//                // Right
-//                if (y + 1 < dumbos[x].Count())
-//                    UpDumboPosition(ref dumbos, x, y + 1);
-
-//                // Down Left
-//                if (x + 1 < dumbos.Count() && y - 1 >= 0)
-//                    UpDumboPosition(ref dumbos, x + 1, y - 1);
-
-//                // Down
-//                if (x + 1 < dumbos.Count())
-//                    UpDumboPosition(ref dumbos, x + 1, y);
-
-//                // Down Right
-//                if (x + 1 < dumbos.Count() && y + 1 < dumbos[x].Count())
-//                    UpDumboPosition(ref dumbos, x + 1, y + 1);
-
-//            }
-//        }
-
-//    }
-
-//    internal class Day11Dumbo
-//    {
-//        public int EnergyLevel { get; set; } = 0;
-//        public bool Flashed { get; set; } = false;
-//    }
-//}
